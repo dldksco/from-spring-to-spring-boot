cbff7fd6ecf39ee4bf31173746f8703ce76db724
ianchae <dldksco@naver.com>
2023-06-27 15:09:58 +0900

    [feat] Servlet container 생성

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,22 @@
 package tobyspring.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+
+		//톰캣 외의 제티 같은 다른 웹서버도 이용가능
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
+
 	}
 
 }
