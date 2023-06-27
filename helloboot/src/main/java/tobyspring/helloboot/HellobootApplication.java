0388db92a84b6be6a6ad264d959a827227374371
ianchae <dldksco@naver.com>
2023-06-27 16:51:14 +0900

    [feat] Front controller 구현

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -26,17 +27,31 @@ public class HellobootApplication {
 		//톰캣 외의 제티 같은 다른 웹서버도 이용가능
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				//요청과 응답을 만드는데 필요한 오브젝트를 파라미터로 넣음
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello Servlet"+ name);
+					//인증, 보안과 같은 공통기능을 프론트 컨트롤러가 하고
+					HelloController helloController = new HelloController();
+					//서블릿 컨테이너의 매핑기능을 프론트 컨트롤러가 담당함
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("/hello");
+
+						String ret = helloController.hello(name);
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE,MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}
+					else if(req.getRequestURI().equals("/user")){
+						//
+					}else{
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 
