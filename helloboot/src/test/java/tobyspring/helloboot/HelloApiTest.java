fb0d8c18c94276b6094d04df58ab33c621967bcf
ianchae <dldksco@naver.com>
2023-07-12 20:43:41 +0900

    [test] 컨트롤러단 단위테스트

--- helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -28,4 +28,16 @@ public class HelloApiTest {
         // body Hello Spring
         assertThat(res.getBody()).isEqualTo("HelloSpring");
     }
+    @Test
+    void failsHelloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=",String.class);
+
+        // status
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        // header(content-type) test/plain
+
+    }
 }
