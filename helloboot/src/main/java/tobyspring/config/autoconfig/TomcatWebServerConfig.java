d1c879f6827ab29516224d39f3372953bb49e389
ianchae <dldksco@naver.com>
2023-08-03 22:06:46 +0900

    @Conditional 학습

--- helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,11 +2,12 @@ package tobyspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
     /**
@@ -17,5 +18,12 @@ public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory serverFactory(){
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
